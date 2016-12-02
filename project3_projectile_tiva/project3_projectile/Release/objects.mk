5510d26bb4b6aabba7f0a2775efa20eb706089d0
davdandlv <davdandlv@sbcglobal.net>
2016-12-01 22:12:33 -0800

    removing Release folder

--- project3_projectile_tiva/project3_projectile/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/TivaWare/driverlib/ccs/Debug/driverlib.lib"
-
